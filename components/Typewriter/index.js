e74a07d2e6e9183c028f48995fc093ad310ad4c2
Stenio Oliveira <steniooliv@gmail.com>
2021-05-15 14:45:32 -0300

    create Typewriter component

---
+++ components/Typewriter/index.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
+
+export default function Typewriter(props) {
+
+  const [phrase, setPhrase] = useState('');
+  useEffect(() => {
+    let currentText = '';
+    props.text.split('').forEach((char, index) => {
+      setTimeout(() => {
+        currentText = currentText.slice(0,-1);
+        currentText += char;
+        if(props.text.length != index + 1){
+          currentText += "|";
+        }
+        setPhrase(currentText);
+      }, 200 + (index * 100));
+    });
+  }, []);
+
+  return (
+    <>
+      {phrase}
+    </>
+  );
+}
