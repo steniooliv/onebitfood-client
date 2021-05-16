982ec1c9bdfbe920ffcc77ad2f560977a9e80c38
Stenio Oliveira <steniooliv@gmail.com>
2021-05-16 14:40:30 -0300

    create categories component

---
+++ components/Categories/index.js
@@ -0,0 +1,40 @@
+import Slick from 'react-slick';
+import slick_settings from './slick_settings';
+import { Card, Col, Spinner, Alert } from 'react-bootstrap';
+import Slider from 'react-slick';
+import Category from './Category';
+import  getCategories from '../../services/getCategories';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+export default function Categories() {
+  const { categories, isLoading, isError } = getCategories();
+
+  function renderContent() {
+    if(isError)
+      return <Col><Alert variant='custom-red'>Erro ao carregar</Alert></Col>
+    else if(isLoading)
+      return <Col><Spinner animation='border'/></Col>
+    else if(categories.lenght == 0)
+      return <Col>Nenhuma categoria disponível ainda...</Col>
+    else
+      return (
+        <Card className="mt-2">
+          <Slider {...slick_settings} className="px-4 pt-4 text-center">
+            {categories.map((category, i) => <Category {...category} key={i}/>)}
+          </Slider>
+        </Card>
+      );
+  }
+
+  return (
+    <>
+      <h3 className="fw-bold">Categorias</h3>
+      {renderContent()}
+      
+    </>
+  );
+}
